be99d60d45cc3a6ee1f50f8c504eb9a31056c257
Daylen Yang <services@daylenyang.com>
2014-10-07 00:37:40 -0700

    Front-end improvements
    Logo added
    Loading indicator
    Fallback if no location found
    Stop selector

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,11 @@
-var stop_id = null;
+var gStopId = null;
+var gCurrentLocation = null;
+var gAllStops = {};
 
-function time_diff (time) {
+/*
+time - Integer
+*/
+function getTimeDifference(time) {
 	if (!time) {
 		return "";
 	}
@@ -19,27 +24,27 @@ function time_diff (time) {
 	}
 }
 
-function add_cell (info) {
+/*
+info - Object
+*/
+function addCell (info) {
 	var html = '<div class="next-cell"><div class="next-left"><div class="next-line">';
 	html += info.line;
-	html += '</div><div class="next-line-note">';
-	html += info.line_note;
+	html += '</div><div class="next-line-note"><span class="glyphicon glyphicon-circle-arrow-right"></span>';
+	html += ' ' + info.line_note;
 	html += '</div></div><div class="next-right"><div class="next-first">';
-	html += time_diff(info.times[0]);
+	html += getTimeDifference(info.times[0]);
 	html += '</div><div class="next-second">';
-	html += time_diff(info.times[1]);
+	html += getTimeDifference(info.times[1]);
 	html += '</div></div></div>';
 	$('#next-bus').append(html);
 }
 
-function populate_next_bus () {
-	if (!stop_id) return;
+function refreshNextBusCells() {
+	if (!gStopId) return;
 	// Now look up the stop
 	var time = (new Date()).getTime();
-	if (window.location.hash === '#demo') {
-		time += (1000 * 60 * 60 * 24);
-	}
-	$.get('/api/v1/stop/' + stop_id, {'time': time}, function(data) {
+	$.get('/api/v1/stop/' + gStopId, {'time': time}, function(data) {
 		var next = data.next;
 
 		$('#next-bus').html('');
@@ -48,69 +53,118 @@ function populate_next_bus () {
 		} else {
 			$('#no-next-bus').fadeOut();
 			for (var i = 0; i < next.length; i++) {
-				add_cell(next[i]);
+				addCell(next[i]);
 			}
 		}
 	});
 }
 
-setInterval(populate_next_bus, 60000);
+setInterval(refreshNextBusCells, 60000);
 
-function location_success (position) {
-	var lat = position.coords.latitude;
-	var lon = position.coords.longitude;
+function locationSuccess(position) {
+	gCurrentLocation = position;
+	loadDepartureBrowser(null, position);
+}
 
-	$.get('/api/v1/stops', {'lat': lat, 'lon': lon}, function(data) {
-		var stop = data[0];
-		$('#stop-name').text(stop.name);
-		$('#stop-dist').text(stop.dist.toFixed(2));
+function locationError(err) {
+	loadDepartureBrowser(err, null);
+}
 
-		var map_options = {
-			center: {
-				lat: stop.lat,
-				lng: stop.lon
-			},
-			draggable: false,
-			zoom: 15
-		};
-
-		var map = new google.maps.Map(document.getElementById('map'), map_options);
-
-		var image = {
-			url: '/img/curr-loc-small.png',
-			scaledSize: new google.maps.Size(15, 15)
-		};
-
-		var curr_loc = new google.maps.Marker({
-			icon: image,
-			position: new google.maps.LatLng(lat, lon),
-			map: map,
-			title: "Current Location"
-		});
-
-		var stop_marker = new google.maps.Marker({
-			position: new google.maps.LatLng(stop.lat, stop.lon),
-			map: map,
-			title: stop.name,
-			animation: google.maps.Animation.BOUNCE
-		});
-
-		stop_id = stop.id;
-		populate_next_bus();
+function loadDepartureBrowser(err, position) {
+	$('#loading').fadeOut(200, function() {
+		$('#main').fadeIn();
+		if (err) $('#error').show();
+		populateDropdown(position);
 	});
 }
 
-function location_error (err) {
-	$('#stop-name').text("Error");
-	if (err.code == 1) {
-		alert('Could not determine your location because you did not give permission.');
-	} else {
-		alert('Could not determine your location.');
+function populateDropdown(position) {
+	var params = {};
+	if (position) {
+		params.lat = position.coords.latitude;
+		params.lon = position.coords.longitude;
 	}
+	$.get('/api/v1/stops', params, function(data) {
+		for (var i = 0; i < data.length; i++) {
+			gAllStops[data[i].id] = data[i];
+		}
+
+		$('#stop-select').selectize({
+			valueField: 'id',
+			labelField: 'name',
+			searchField: ['name'],
+			sortField: [],
+			options: data,
+			render: {
+				item: function(item) {
+					var str = '<div><b>' + item.name + '</b>';
+					if (item.dist) {
+						str += '<span class="text-muted"> ' + item.dist.toFixed(2) + ' miles away</span>';
+					}
+					return str + '</div>';
+				},
+				option: function(item) {
+					var str = '<div><div>' + item.name + '</div>';
+					if (item.dist) {
+						str += '<div class="text-muted small">' + item.dist.toFixed(2) + ' miles away</div>';
+					}
+					return str + '</div>';
+				}
+			}
+		});
+
+
+		if (data[0].dist) {
+			$('#stop-select')[0].selectize.addItem(data[0].id);
+			loadStopInfo(data[0].id);
+		}
+
+		$('#stop-select')[0].selectize.on('change', loadStopInfo);
+
+	});
+}
+
+function loadStopInfo(stopId) {
+	gStopId = stopId;
+	refreshNextBusCells();
+	loadMap(gAllStops[stopId]);
+}
+
+function loadMap(stop) {
+	var map_options = {
+		center: {
+			lat: stop.lat,
+			lng: stop.lon
+		},
+		zoom: 15
+	};
+
+	var map = new google.maps.Map(document.getElementById('map'), map_options);
+
+	var image = {
+		url: '/img/curr-loc-small.png',
+		scaledSize: new google.maps.Size(15, 15)
+	};
+
+	if (gCurrentLocation) {
+		var curr_loc = new google.maps.Marker({
+			icon: image,
+			position: new google.maps.LatLng(gCurrentLocation.coords.latitude, gCurrentLocation.coords.longitude),
+			map: map,
+			title: "Current Location"
+		});
+	}
+
+	var stop_marker = new google.maps.Marker({
+		position: new google.maps.LatLng(stop.lat, stop.lon),
+		map: map,
+		title: stop.name,
+		animation: google.maps.Animation.DROP
+	});
 }
 
 $(document).ready(function() {
-	navigator.geolocation.getCurrentPosition(location_success, location_error, {
+	navigator.geolocation.getCurrentPosition(locationSuccess, locationError, {
 		enableHighAccuracy: true,
 		timeout: 10000
 	});
